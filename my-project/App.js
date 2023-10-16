1de7ebf09cf9a37f1412927ad78369790b424cf2
RNWalker <rebecca.noelia.walker@gmail.com>
2023-10-16 11:19:16 +0100

    added header

--- my-project/App.js
+++ my-project/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './homepage/HomepageHeader';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Mind Mentor to start working on your app!</Text>
+    <Header/>
+      {/* <Text>Mind Mentor to start working on your app!</Text> */}
       <StatusBar style="auto" />
     </View>
   );
