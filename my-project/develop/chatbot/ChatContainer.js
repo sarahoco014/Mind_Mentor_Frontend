2ccbcf43565cc3f956c09c7c266f864bad6941c4
Sandra <sandramtzd@hotmail.com>
2023-10-17 11:40:15 +0100

    added chat container code

---
+++ my-project/develop/chatbot/ChatContainer.js
@@ -0,0 +1,46 @@
+
+
+
+
+const ChatContainer = () => {
+    
+    const [chats, setChats] = useState([]);
+
+    const fetchChats = async () => {
+        const response = await fetch("http://localhost:8080/chats/1")
+        const data = await response.json();
+        setChats(data);
+    }
+
+    const postMessage = async () => {
+
+        
+
+        const requestOptions = {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            text: "message"
+
+          })
+        };
+        // POST request
+        
+        const response = await fetch("http://localhost:8080/messages?chatId=1", requestOptions)}
+    
+    useEffect(()=> {fetchChats()}, []);
+    
+
+
+    return (
+        <View>
+
+        </View>
+            
+
+    )
+}
+
+
+
+export default ChatContainer;
