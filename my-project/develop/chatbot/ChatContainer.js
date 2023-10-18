ee70a4dcb96b2ecfc8bc3b36ebfe94b824a1319d
sarahoco014 <soconnor2@outlook.com>
2023-10-18 09:36:56 +0100

    changes local host to ensure fetch is working on emulator

--- my-project/develop/chatbot/ChatContainer.js
+++ my-project/develop/chatbot/ChatContainer.js
@@ -11,7 +11,7 @@ const ChatContainer = () => {
 
     const fetchAllChats = async () => {
         try {
-            const response = await fetch("http://localhost:8080/chats/all");
+            const response = await fetch("http://10.0.2.2:8080/chats/all");
             const data = await response.json();
             setChats(data);
             setLoading(false);
@@ -35,7 +35,7 @@ const ChatContainer = () => {
         };
         // POST request
         
-        const response = await fetch("http://localhost:8080/messages?chatId=1", requestOptions)}
+        const response = await fetch("http://10.0.2.2:8080/messages?chatId=1", requestOptions)}
     
     useEffect(()=> {fetchAllChats()}, []);
     
