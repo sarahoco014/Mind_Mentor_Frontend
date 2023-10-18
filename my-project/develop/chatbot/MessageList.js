3af6818cbcde174a56d36b7ff3cf1dfa5d258476
sarahoco014 <soconnor2@outlook.com>
2023-10-18 10:59:06 +0100

    fixes bot and user message styling

--- my-project/develop/chatbot/MessageList.js
+++ my-project/develop/chatbot/MessageList.js
@@ -1,40 +1,27 @@
 import { View, Text, StyleSheet } from "react-native";
 import BotMessage from "./BotMessage";
 import UserMessage from "./UserMessage";
+import messageStyle from "./MessageStyle";
 
 const MessageList = ({ chat }) => {
   if (!chat || !chat.messages) {
     return <Text>No messages available</Text>;
   }
 
-
-  const style = StyleSheet.create({
-    bot: {
-        color: 'red'
-    },
-    user: {
-        color: 'blue'
-    }
-})
-
   const mappedChats = chat.messages.map((message, index) => {
     if (message.bot === true) {
       return (
-        <View 
-        style={style.bot}
-        key={index}>
-          <BotMessage
-        message={message.message} />
-        </View>
+        <BotMessage
+        style={messageStyle.bot}
+        message={message.message}
+        key={index} />
       );
     } else {
       return (
-        <View 
-        style={style.user}
-        key={index}>
-          <UserMessage
-          message={message.message} />
-        </View>
+        <UserMessage
+        style={messageStyle.user}
+        message={message.message}
+        key={index} />
       );
     }
   });
---
+++ my-project/develop/chatbot/messageStyle.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+const messageStyle = StyleSheet.create({
+    bot: {
+        color: 'red'
+    },
+    user: {
+        color: 'blue'
+    }
+})
+
+export default messageStyle;
