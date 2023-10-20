e42437dff90d066754e99e28809d0aef481e417c
Sandra <sandramtzd@hotmail.com>
2023-10-20 21:14:42 +0100

    added onboarding screens

--- my-project/App.js
+++ my-project/App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import HomepageHeader from './develop/homepage/HomepageHeader';
 import HomepageContainer from './develop/homepage/HomepageContainer';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -9,12 +9,112 @@ import ResourcesContainer from './develop/resources/ResourcesContainer';
 import ChatContainer from './develop/chatbot/ChatContainer';
 import Emojis from './develop/homepage/Emojis';
 import ContactsModal from './develop/resources/modals/ContactsModal';
+import AppIntroSlider from 'react-native-app-intro-slider';
+import {COLORS, SIZES} from './src/constants/theme';
+
 
 
 const Stack = createStackNavigator();
 
+const Slides = [
+  {
+    id: 1,
+    title: 'Mental health at work',
+    description: "Approximately 1 in 6.8 people experience mental health problems in the workplace (14.7%) ",
+    image: require('./src/assets/images/onboarding1.png')
+
+  },
+  {
+    id: 2,
+    title: 'MindMentor is an AI solution for the workplace',
+    description: "Even on the best days, stress can prevent us from being fully focused and doing our best work. Too much of it can lead to burnout, disengagement, more sick days, and strained relationships in the workplace.",
+    image: require('./src/assets/images/onboarding2.png')
+
+  },
+  {
+    id: 3,
+    title: 'Mental health more important than ever',
+    description: "Nowadays, it's never been more critical for business and Human Resources leaders to address the mental well-being of their teams head-on. Enterprise-ready, MindMentor offers an AI solution to healthier organisations from the inside out.",
+    image: require('./src/assets/images/onboarding3.png')
+
+  }
+
+]
+
 
 export default function App() {
+  const [showHomePage, setShowHomePage] = useState (false)
+
+  const buttonLabel = (label) => {
+    return(
+      <View style={{
+        padding: 12
+      }} >
+        <Text style={{
+          color: COLORS.title,
+          fontWeight: '600',
+          fontSize: SIZES.h4,
+
+        }}>
+          {label}
+        </Text>
+      </View>
+    )
+  }
+  
+  if(!showHomePage) {
+    return (
+      <AppIntroSlider
+      data={Slides}
+      renderItem={({item})=>{
+        return(
+          <View style={{
+            flex: 1,
+            alignItems: 'center',
+            padding: 15,
+            paddingTop:100,
+          }}>
+            <Image
+            source={item.image}
+            style={{
+              width: SIZES.width - 80,
+              height: 400,
+            }}
+            resizeMode="contain"
+            />
+            <Text style={{
+              fontWeight: "bold",
+              color: COLORS.title,
+              fontSize: SIZES.h1,
+              textAlign: 'center',
+            }}>
+              {item.title}
+            </Text>
+            <Text style={{
+              textAlign: 'center',
+              paddingTop: 5,
+              color: COLORS.title
+            }}>
+              {item.description}
+            </Text>
+          </View>
+        )
+      }}
+      activeDotStyle={{
+        backgroundColor: COLORS.primary,
+        width: 30,
+      }}
+      showSkipButton
+      renderNextButton={()=>buttonLabel("Next")}
+      renderSkipButton={()=> buttonLabel("Skip")}
+      renderDoneButton={()=> buttonLabel("Done")}
+      onDone={()=>{
+        setShowHomePage(true);
+      }}
+      />
+    )
+  }
+
   return (
 
           <NavigationContainer>
@@ -28,12 +128,7 @@ export default function App() {
       </NavigationContainer>
 
 
-    // <View style={styles.container}>
-    // <HomepageHeader/>
-    // <HomepageContainer/>
-    //   {/* <Text>Mind Mentor to start working on your app!</Text> */}
-    //   <StatusBar style="auto" />r
-    // </View>
+
   );
 }
 
---
+++ my-project/develop/_layout.js
@@ -0,0 +1,28 @@
+// import { Stack } from 'expo-router/stack';
+// import { useFonts } from "expo-font";
+
+
+// export const unstable_settings = {
+//   // Ensure any route can link back to `/`
+//   initialRouteName: "HomepageContainer",
+// };
+
+// const Layout = () => {
+//   const [fontsLoaded] = useFonts({
+//     MontserratSemiBold: require("../src/assets/fonts/Montserrat-SemiBold.ttf"),
+//     KarlaRegular: require("../assets/fonts/Karla-Regular.ttf"),
+//     MontserratRegular: require("../assets/fonts/Montserrat-Regular.ttf"),
+//   });
+
+//   if (!fontsLoaded) {
+//     return null;
+//   }
+
+//   return (
+//     <Stack initialRouteName="HomepageContainer">
+//       <Stack.Screen name="HomepageContainer" />
+//     </Stack>
+//   )
+// };
+
+// export default Layout;
