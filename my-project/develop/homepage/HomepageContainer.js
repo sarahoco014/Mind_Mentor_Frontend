ab4fc2c4e0e5701852671c69358e4e9bb2ef4f52
sarahoco014 <soconnor2@outlook.com>
2023-10-23 13:22:01 +0100

    adds home page styling

--- my-project/develop/homepage/HomepageContainer.js
+++ my-project/develop/homepage/HomepageContainer.js
@@ -1,27 +1,53 @@
 import React from "react";
-import { View, Button } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import Emojis from "./Emojis";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from '@react-navigation/stack';
+import { COLORS } from "../../src/constants/theme";
 
 const HomepageContainer = ({navigation}) => {
 
-    const Stack = createStackNavigator();
-
-        return (
-   
-         <View>
-            <Button title = "chat button" 
-            onPress={() => navigation.navigate('ChatContainer')}/>
-            <Button title = "resources" 
-            onPress={() => navigation.navigate('ResourcesContainer')}/>
-
-            <Emojis/>
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity style={styles.buttonContainer} onPress={() => navigation.navigate('ChatContainer')}>
+                <Text style={styles.buttonText}>Chat</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.buttonContainer} onPress={() => navigation.navigate('ResourcesContainer')}>
+                <Text style={styles.buttonText}>Resources</Text>
+            </TouchableOpacity>
+            <View style={styles.emojisAndTextContainer}>
+                    <Text>How do you feel today?</Text>
+                <View style={styles.emojisContainer}>
+                    <Emojis/>
+                </View>
+            </View>
         </View>
-
-        )
-    
-
+    )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        paddingHorizontal: 15,
+        },
+    buttonContainer: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 50,
+        margin: 30,
+        borderRadius: 20,
+        elevation: 3,
+        backgroundColor: COLORS.primary,
+    },
+    buttonText: {
+        fontSize: 20,
+        lineHeight: 21,
+        fontWeight: 'bold',
+        letterSpacing: 0.25,
+        color: COLORS.jet,
+        fontFamily: 'Montserrat_600SemiBold'
+    }
+});
 
 export default HomepageContainer;
